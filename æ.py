3077be1f56044db48a7b26ba437a24b63711f014
GlutenFreeGrapes <daniellian238@gmail.com>
2022-01-16 11:52:14 -0500

    skip questions and disable buzzer
    so now when the question goes dead the number of tossups updates, and the function for skipping questions also works now
    onto working on bonuses

--- æ.py
+++ æ.py
@@ -1,4 +1,5 @@
 
+from logging import root
 import tkinter as tk
 ccccc=["Current Events", "Fine Arts", "Geography", "History", "Literature", "Mythology", "Philosophy", "Religion", "Science", "Social Science", "Trash"]
 sssssccccc={"Current Events Subcategories":["American Current Events", "Other Current Events"], "Fine Arts Subcategories":["American Fine Arts", "Audiovisual Fine Arts", "Auditory Fine Arts", "British Fine Arts", "European Fine Arts", "Opera", "Visual Fine Arts", "World Fine Arts", "Other Fine Arts"], "Geography Subcategories":["American Geography", "World Geography"], "History Subcategories":["American History", "British History", "European History", "Classical History", "World History", "Other History"], "Literature Subcategories":["American Literature", "British Literature", "European Literature", "Classical Literature", "World Literature", "Other Literature"], "Mythology Subcategories":["American Mythology", "Chinese Mythology", "Egyptian Mythology", "Greco-Roman Mythology", "Indian Mythology", "Japanese Mythology", "Norse Mythology", "Other East Asian Mythology", "Other Mythology"], "Philosophy Subcategories":["American Philosophy", "Classical Philosophy", "East Asian Philosophy", "European Philosophy", "Other Philosophy"], "Religion Subcategories":["American Religion", "Christianity", "East Asian Religion", "Islam", "Judaism", "Other Religion"], "Science Subcategories":["American Science", "Biology", "Chemistry", "Computer Science", "Math", "Physics", "World Science", "Other Science"], "Social Science Subcategories":["American Social Science", "Anthropology", "Economics", "Linguistics", "Political Science", "Psychology", "Sociology", "Other Social Science"], "Trash Subcategories":["American Trash", "Movies", "Music", "Sports", "Television", "Video Games", "Other Trash"]}
@@ -8,19 +9,36 @@ buzzed=False
 reading=False
 dead=False
 ansalrgiven=False
+qskipped=False
 timeoutctr=None
 endctr=None
+qctr=None
 tu=0
 bon=0
 ptn=[0,0,0]
 bagels=[0,0,0,0]
 tupts=0
 bpts=0
-qnum=-1
+tunum=-1
+bonnum=-1
+subbonnum=0
 pm=0
 curwd=0
-qlist=["""A leader of the fief of Kii who led the country during this period introduced sweet potato and sugarcane cultivation and began the compilation of the Kansei law code. Scholarly endeavors in this era were divided into the schools of Ancient Learning, National Learning, and Dutch Learning. This era included the artistic flourishing of the "original happiness period." Another ruler's attempt to stamp out Christianity during this era led to the (*)) Shimabara Rebellion. During the disintegration of its namesake government, this period saw the establishment of the Ezo Republic and the Boshin War. Ended by the proclamation of the Charter Oath and rallying behind Emperor Meiji, for 10 points, identify this final Japanese shogunate which lasted from 1600 to 1858.""", """Two wolves named "Greedy" and "Ravenous" devour the food that this deity doesn't eat. This deity can see far from his throne of Hlidskjalf, and has information brought to him by the ravens Huginn and Muninn. This deity hung on his spear Gungnir from the World Tree Yggdrasil in order to master the runes, and he gave up one of his eyes to drink from Mimir's well. He rides the eight-legged steed Sleipnir and will be swallowed whole by Fenrir at Ragnarok. For 10 points, name this owner of Valhalla, the chief god of the Norse pantheon."""]
-alist=["Tokugawa Shogunate","Odin"]
+tuct=None
+tossuppts=None
+ppg=None
+ptnct=None
+bonct=None
+bonuspts=None
+ppb=None
+tttb=None
+root=None
+buzzer=None
+tulist=["""A leader of the fief of Kii who led the country during this period introduced sweet potato and sugarcane cultivation and began the compilation of the Kansei law code. Scholarly endeavors in this era were divided into the schools of Ancient Learning, National Learning, and Dutch Learning. This era included the artistic flourishing of the "original happiness period." Another ruler's attempt to stamp out Christianity during this era led to the (*)) Shimabara Rebellion. During the disintegration of its namesake government, this period saw the establishment of the Ezo Republic and the Boshin War. Ended by the proclamation of the Charter Oath and rallying behind Emperor Meiji, for 10 points, identify this final Japanese shogunate which lasted from 1600 to 1858.""", """Two wolves named "Greedy" and "Ravenous" devour the food that this deity doesn't eat. This deity can see far from his throne of Hlidskjalf, and has information brought to him by the ravens Huginn and Muninn. This deity hung on his spear Gungnir from the World Tree Yggdrasil in order to master the runes, and he gave up one of his eyes to drink from Mimir's well. He rides the eight-legged steed Sleipnir and will be swallowed whole by Fenrir at Ragnarok. For 10 points, name this owner of Valhalla, the chief god of the Norse pantheon."""]
+tualist=["Tokugawa Shogunate","Odin"]
+bonlist=[["""In 2013, this man was succeeded as Director of the FBI by James Comey, before being appointed to his current post by Deputy Attorney General Rod Rosenstein. For 10 points each:\nName this Republican Special Counsel for the Department of Justice, who is currently overseeing an investigation into Russian interference in the 2016 US Presidential Election.""", """This former Trump campaign advisor entered a plea deal with Mueller, which resulted in additional charges for him and Paul Manafort, such as conspiracy against the United States from when they lobbied for Ukraine.""","""Gates and Manafort were lobbyists for this pro-Russian Ukrainian Party. Former President Viktor Yanukovych was the first member of this party to be elected president before leaving office in 2014."""],["""In this text, a woman is impregnated by eating a lingonberry and gives birth to a son who becomes King of Karelia. For 10 points each:\nName this epic which contains the story of the virgin Marjatta. In another story from this epic, a comb begins to bleed after its hero drowns trying to capture a swan to win Louhi's daughter's hand in marriage.""","""The story of Marjatta symbolizes the Christianization of this European country, which reveres Ilmarinen and Väinämöinen as mythological heroes and regards the Kalevala as its national epic.""","""In Finland, the word for Satan, “saatana,” is frequently used as a swear word in conjunction with a word thought to refer to this chief Finnish god. This god of the sky conjured lightning from a hammer, axe or sword."""]]
+bonalist=[["""Robert Mueller""", """Rick Gates""","""Party of Regions"""],["""Kalevala""","""Finland""","""Ukko"""]]
+
 tbrn=0
 
 class ToolTip(object):
@@ -161,7 +179,7 @@ def setup():
 
 
 def qscreen(tuorbon,timeint):
-    global buzzed
+    global buzzed,root,tuct,tossuppts,ppg,ptnct,bonct,bonuspts,ppb,tttb,buzzer
     root=tk.Tk()
     root.geometry("+20+20")
     topf=tk.Frame(root)
@@ -181,7 +199,7 @@ def qscreen(tuorbon,timeint):
     buzzed=False
     tuct=tk.Label(statframe,text='Tossups: %s'%(tu))
     tossuppts=tk.Label(statframe,text='Tossup Points: %s'%(tupts))
-    ppg=tk.Label(statframe,text='PP20TUH: %s'%(0 if tu==0 else tupts/tu))
+    ppg=tk.Label(statframe,text='PP20TUH: %s'%(0 if tu==0 else tupts/tu*20))
     ptnct=tk.Label(statframe,text='Powers/10s/Negs: %s'%('/'.join(str(i) for i in ptn)))
     bonct=tk.Label(statframe,text='Bonuses: %s'%(bon))
     bonuspts=tk.Label(statframe,text='Bonus Points: %s'%(bpts))
@@ -198,17 +216,17 @@ def qscreen(tuorbon,timeint):
         read['state']='disabled'
         timeoutctr=root.after(7500,checkanswer)
     def checkanswer():
-        global tu,tupts,bon,bpts,ptn,bagels,ansalrgiven,timeoutctr
-        if not ansalrgiven:
+        global tu,tupts,bon,bpts,ptn,bagels,ansalrgiven,timeoutctr,reading,tuct,tossuppts,ppg,ptnct,bonct,bonuspts,ppb,tttb
+        if not ansalrgiven and not dead:
             root.after_cancel(timeoutctr)
             if endctr!=None:
                 qframe.after_cancel(endctr)
             givenans=is_this_correct.get()
             ansalrgiven=True
             answerline.delete(0,len(givenans))
-            actualans=alist[qnum]
+            actualans=tualist[tunum]
             if buzzed:
-                if givenans.lower() == actualans.lower():
+                if givenans.lower() == actualans.lower():#improve sometime
                     if curwd<=pm:
                         tupts+=15
                         ptn[0]+=1
@@ -217,21 +235,22 @@ def qscreen(tuorbon,timeint):
                         ptn[1]+=1
                 else:
                     if reading:
-                        tupts-=5
+                        tupts-=5# add "were you correct"
                         ptn[2]+=1
                 if tbrn==0:
                     tu+=1
-                    qcanvas.itemconfigure(qtext,text=qlist[qnum]+'\n\n'+alist[qnum])
+                    qcanvas.itemconfigure(qtext,text=tulist[tunum]+'\n\n'+tualist[tunum])
                     root.update()
                     if tuorbon==0:
                         answerline['state']='disabled'
                         enterans['state']='disabled'
                         read['state']='normal'
+                reading=False
                 tuct['text']='Tossups: %s'%(tu)
                 tossuppts['text']='Tossup Points: %s'%(tupts)
-                ppg['text']='PP20TUH: %s'%(0 if tu==0 else tupts/tu)
+                ppg['text']='PP20TUH: %s'%(0 if tu==0 else tupts/tu*20)
                 ptnct['text']='Powers/10s/Negs: %s'%('/'.join(str(i) for i in ptn))
-    enterans=tk.Button(controlframe,text="Enter",command=checkanswer)
+    enterans=tk.Button(controlframe,text="Enter",command=checkanswer,state='disabled')
     enterans.grid(row=0,column=4)
     buzzer=tk.Button(controlframe,text="Buzz",command=buzzin)
     if reading==False:
@@ -244,26 +263,56 @@ def qscreen(tuorbon,timeint):
     timeo.grid(row=0,column=0)
     thyme.grid(row=0,column=1)
     def readq():
-        global reading,buzzed,qnum,dead,ansalrgiven
-        reading=True
-        buzzed=False
-        dead=False
-        ansalrgiven=False
-        qnum+=1
-        if qnum>=len(qlist):
-            qcanvas.itemconfigure(qtext, text = "No more questions left 😔")
-            return
-        if reading and tuorbon==0 or tuorbon==2:
-            buzzer['state']='normal'
-        if tuorbon==0:
-            read_tossup(qframe,qcanvas,qtext,thyme)
-        elif tuorbon==1:
-            read_bonus(qframe,qcanvas,qtext,thyme)
-        else:
-            if tbrn==0:
+        global reading,buzzed,tunum,dead,ansalrgiven,qskipped,bonnum
+        if not reading:
+            reading=True
+            buzzed=False
+            dead=False
+            ansalrgiven=False
+            qskipped=False
+            tunum+=1
+            bonnum+=1
+            if tuorbon==0:
+                if tunum>=len(tulist):
+                    qcanvas.itemconfigure(qtext, text = "No more questions left 😔\nPress [Quit] to exit")
+                    read['state']='disabled'
+                    thyme['state']='disabled'
+                    return
+                if reading and tbrn==0:
+                    buzzer['state']='normal'
+                qcanvas.itemconfigure(qtext, font=("times new roman", 15))
                 read_tossup(qframe,qcanvas,qtext,thyme)
-            else:
+            elif tuorbon==1:
+                if reading and tbrn==1:
+                    buzzer['state']='disabled'
+                    answerline['state']='normal'
+                qcanvas.itemconfigure(qtext, font=("times new roman", 13))
                 read_bonus(qframe,qcanvas,qtext,thyme)
+            else:
+                if tbrn==0:
+                    read_tossup(qframe,qcanvas,qtext,thyme)
+                else:
+                    read_bonus(qframe,qcanvas,qtext,thyme)
+        else:
+            qskipped=True
+            qframe.after_cancel(qctr)
+            tunum+=1
+            if tunum>=len(tulist):
+                qcanvas.itemconfigure(qtext, text = "No more questions left 😔\nPress [Quit] to exit")
+                read['state']='disabled'
+                thyme['state']='disabled'
+                buzzer['state']='disabled'
+                return
+            qskipped=False
+            if tuorbon==0:
+                read_tossup(qframe,qcanvas,qtext,thyme)
+            elif tuorbon==1:
+                read_bonus(qframe,qcanvas,qtext,thyme)
+            else:
+                if tbrn==0:
+                    read_tossup(qframe,qcanvas,qtext,thyme)
+                else:
+                    read_bonus(qframe,qcanvas,qtext,thyme)
     read=tk.Button(controlframe,text="Next/Skip",command=readq)
     read.grid(row=0,column=0)
     if tuorbon==0:
@@ -289,13 +338,22 @@ def qscreen(tuorbon,timeint):
     buzzer.grid(row=0,column=2)
     qbt.grid(row=0,column=1,sticky='e')
     root.mainloop()
-def check_if_buzz_at_eoq():
-    global dead,tu
+def check_if_buzz_at_eotu():
+    global dead,tu,tuct,tossuppts,ppg,ptnct,root,buzzer
     if reading==False and buzzed==False:
+        dead=True
+        tu+=1
+        tuct['text']='Tossups: %s'%(tu)
+        ppg['text']='PP20TUH: %s'%(0 if tu==0 else tupts/tu*20)
+        buzzer['state']='disabled'
+        root.update()
+def check_if_buzz_at_eobon():
+    global dead,bon,bonct,bonuspts,ppb,tttb
+    if reading==False and ansalrgiven==False:
         dead=True
 def read_tossup(window,canvas,question_txt,timeint):
     global pm
-    current_q = qlist[qnum]
+    current_q = tulist[tunum]
     powermark=current_q.find("(*)")
     words=current_q.split()
     if powermark>-1:
@@ -305,25 +363,42 @@ def read_tossup(window,canvas,question_txt,timeint):
         words[index-1]+=" (*)"
     else:
         pm=-1
-    iterative(words, 0, window,canvas,question_txt,timeint)
-def iterative(words, i, window,canvas,question_txt,timeint):
-    global curwd
+    itertu(words, 0, window,canvas,question_txt,timeint)
+def itertu(words, i, window,canvas,question_txt,timeint):
+    global curwd,qctr
     curwd=i-1
     if i > len(words):
         global reading,endctr
         reading=False
-        endctr=window.after(5000,check_if_buzz_at_eoq)
+        endctr=window.after(5000,check_if_buzz_at_eotu)
+        return
+    if qskipped:
         return
     if not buzzed:
         canvas.itemconfigure(question_txt, text=' '.join(words[:i]))
         i += 1
-        window.after(timeint.get(), lambda: iterative(words, i,window,canvas,question_txt,timeint))
+        qctr = window.after(timeint.get(), lambda: itertu(words, i,window,canvas,question_txt,timeint))
 
 def read_bonus(window,canvas,question_txt,timeint):
-    current_q = """A leader of the fief of Kii who led the country during this period introduced sweet potato and sugarcane cultivation and began the compilation of the Kansei law code. Scholarly endeavors in this era were divided into the schools of Ancient Learning, National Learning, and Dutch Learning. This era included the artistic flourishing of the "original happiness period." Another ruler's attempt to stamp out Christianity during this era led to the (*)) Shimabara Rebellion. During the disintegration of its namesake government, this period saw the establishment of the Ezo Republic and the Boshin War. Ended by the proclamation of the Charter Oath and rallying behind Emperor Meiji, for 10 points, identify this final Japanese shogunate which lasted from 1600 to 1858."""
-
+    current_q = bonlist[bonnum]
+    print(current_q)
+    bonwords=current_q[subbonnum].split()
+    itertu(bonwords, 0, window,canvas,question_txt,timeint)
+def iterbon(words, i, window,canvas,question_txt,timeint):
+    if i > len(words):
+        global reading,endctr
+        reading=False
+        endctr=window.after(10000,check_if_buzz_at_eobon)
+        return
+    if qskipped:
+        return
+    if not buzzed:
+        canvas.itemconfigure(question_txt, text=' '.join(words[:i]))
+        i += 1
+        qctr = window.after(timeint.get(), lambda: iterbon(words, i,window,canvas,question_txt,timeint))
 setup()
+if ttbb<2:
+    tbrn=ttbb
+else:
+    tbrn=0
 qscreen(ttbb,tthhyymmee)
-
-# make new tk and display w text box, pp20tuh and ppb
-#buzz w button and anwer w text box, give 5-7s to ans
