97a738505363acbfac2293a1ca5d38d32127d176
GlutenFreeGrapes <daniellian238@gmail.com>
2022-04-09 17:12:48 -0400

    fixed valid answers thing
    i've figured out how (at least yale certamen) marks all their answers and have used it to generate all valid answers given a single string. totally didnt spend 8 hours on it

--- CReader.py
+++ CReader.py
@@ -390,7 +390,7 @@ def qscreen(tuorbon,timeint):
             if tbrn==0:
                 actualans=tualist[tunum]
                 if buzzed:
-                    if close_enough(givenans.lower(),actualans.lower()):
+                    if close_enough(givenans.lower(),actualans):
                         tupts+=10
                         ptn[0]+=1
                         tustatus.append(10)
@@ -449,7 +449,7 @@ def qscreen(tuorbon,timeint):
                 enterans['state']='disabled'
                 root.unbind("<Return>")
                 actualans=bonalist[bonnum][subbonnum]
-                if close_enough(givenans.lower(),actualans.lower()):
+                if close_enough(givenans.lower(),actualans):
                     curbpts+=5
                     subbonstatus.append(5)
                 else:
@@ -705,15 +705,8 @@ def qscreen(tuorbon,timeint):
     root.bind('<Control-w>',lambda event:leavereader())
     root.focus_force()
     def close_enough(given,normalans):
-        nans=normalans.replace('[accept equivalents]','')
-        possans=set()
-        n=nans.split('/')
-        if n!=None:
-            possans.update({i.strip() for i in n if i!=''})
-        n=nans.split(' or ')
-        if n!=None:
-            possans.update({i.strip() for i in n if i!=''})
-        possans.update({ignorparens(i).strip() for i in possans})
+        nans=removesqb(normalans.replace(' or ','/').replace('//','/')).lower()
+        possans=allposans(nans)
         for i in possans:
             if enchant.utils.levenshtein(given,i)<min(3,len(i)//2):
                 return True
@@ -740,7 +733,7 @@ def qscreen(tuorbon,timeint):
         ans=is_this_correct.get()
         if ans!="":
             answerline.delete(0,len(ans))
-            if close_enough(ans.lower(),bonalist[bonnum][subbonnum].lower()):
+            if close_enough(ans.lower(),bonalist[bonnum][subbonnum]):
                 curbpts+=5
                 subbonstatus.append(5)
             else:
@@ -825,28 +818,159 @@ def prompt(gans,aans):
         if gans.strip()=='idk' or gans.strip()=='' or (gans.strip()=="n" and aans!="n"):
             return False
         return messagebox.askyesno("Were you correct?","Were you correct? (Press y/n)\nYour answer: %s\nActual answer: %s"%(gans,aans))
-def ignorparens(s):
-    parenpairs=set()
-    pp=set()
-    lparen=[]
+def allparspos(opt,pars,s):
+    app=[s[:pars[0][0]]]
+    for i in range(len(pars)-1):
+        app.append(s[pars[i][1]+1:pars[i+1][0]])
+    app.append(s[pars[-1][1]+1:])
+    if len(app)==len(opt):
+        app.append('')
+    fr=[(app[0],0)]
+    v=set()
+    while len(fr)>0:
+        l,n=fr.pop()
+        if n<len(opt):
+            if l+app[n+1] not in v:
+                fr.append((l+app[n+1],n+1))
+            for jjj in opt[n]:
+                if l+jjj+app[n+1] not in v:
+                    fr.append((l+jjj+app[n+1],n+1))
+        else:
+            v.add(l.replace('  ',' ').strip())
+    return v
+def allbracpos(opt,brac,s):
+    app=[s[:brac[0][0]]]
+    for i in range(len(brac)-1):
+        app.append(s[brac[i][1]+1:brac[i+1][0]])
+    app.append(s[brac[-1][1]+1:])
+    if len(app)==len(opt):
+        app.append('')
+    fr=[(app[0],0)]
+    v=set()
+    while len(fr)>0:
+        l,n=fr.pop()
+        if n<len(opt):
+            for jjj in opt[n]:
+                if l+jjj+app[n+1] not in v:
+                    fr.append((l+jjj+app[n+1],n+1))
+        else:
+            v.add(l.replace('  ',' ').strip())
+    return v
+def allmixpos(opt,mix,s):
+    app=[(s[:mix[0][0]],s[mix[0][0]])]
+    for i in range(len(mix)-1):
+        app.append((s[mix[i][1]+1:mix[i+1][0]],s[mix[i+1][0]]))
+    app.append((s[mix[-1][1]+1:],''))
+    if len(app)==len(opt):
+        app.append('')
+    fr=[(app[0],0)]
+    v=set()
+    while len(fr)>0:
+        l,n=fr.pop()
+        if n<len(opt):
+            if l[1]=='{':
+                for jjj in opt[n]:
+                    if jjj!='':
+                        if l[0]+jjj+app[n+1][0] not in v:
+                            fr.append(((l[0]+jjj+app[n+1][0],app[n+1][1]),n+1))
+            elif l[1]=='(':
+                fr.append(((l[0]+app[n+1][0],app[n+1][1]),n+1))
+                for jjj in opt[n]:
+                    if jjj!='':
+                        if l[0]+jjj+app[n+1][0] not in v:
+                            fr.append(((l[0]+jjj+app[n+1][0],app[n+1][1]),n+1))
+        else:
+            v.add(l[0].replace('  ',' ').strip())
+    return v
+def allposans(s):
+    if s.find('{')==-1 and s.find('(')==-1 and s.find('/')==-1:
+        return {s}
+    pars=[]
+    brac=[]
+    lp=[]
+    lb=[]
+    p=0
+    c=0
+    slashes=[]
     for n,i in enumerate(s):
-        if i=='(':
-            lparen.append((i,n))
+        if i=='{' and c==0:
+            lb.append(n)
+            c+=1
+        elif i=='{':
+            c+=1
+        elif i=='}' and c==1:
+            brac.append((lb.pop(),n))
+            c-=1
+        elif i=='}':
+            c-=1
+        elif i=='(' and p==0:
+            lp.append(n)
+            p+=1
+        elif i=='(':
+            p+=1
+        elif i==')' and p==1:
+            pars.append((lp.pop(),n))
+            p-=1
         elif i==')':
-            parenpairs.add((lparen.pop()[1],n))
-    for i in parenpairs:
-        for j in parenpairs:
-            if i!=j and i[0]<j[0] and j[1]<i[1]:
-                pp.add(j)
-    parts=sorted([i for i in parenpairs if i not in pp])
-    if len(parts)>0:
-        fstr=s[:parts[0][0]]
-        for i in range(len(parts)-1):
-            fstr+=s[parts[i][1]+1:parts[i+1][0]]
-        fstr+=s[parts[-1][1]+1:]
+            p-=1
+        elif i=='/' and c==0:
+            slashes.append(n)
+    if len(slashes)>0:
+        b=set()
+        b.update(allposans(s[:slashes[0]].strip()))
+        for i in range(len(slashes)-1):
+            b.update(allposans(s[slashes[i]+1:slashes[i+1]].strip()))
+        b.update(allposans(s[slashes[-1]+1:].strip()))
+        return b
+    if len(pars)>0 and len(brac)==0:
+        opt=[]
+        for i in pars:
+            opt.append(allposans(s[i[0]+1:i[1]]))
+        return allparspos(opt,pars,s)
+    elif len(brac)>0 and len(pars)==0:
+        opt=[]
+        for i in brac:
+            opt.append(allposans(s[i[0]+1:i[1]]))
+        return allbracpos(opt,brac,s)
+    elif len(brac)>0 and len(pars)>0:
+        pp=brac.copy()
+        pp.extend(pars)
+        ppp=[]
+        for i in pp:
+            b=True
+            for j in pp:
+                if j!=i and j[0]<i[0] and i[1]<j[1]:
+                    b=False
+            if b:
+                ppp.append(i)
+        ppp=sorted(ppp)
+        opt=[]
+        for i in ppp:
+            opt.append(allposans(s[i[0]+1:i[1]]))
+        return allmixpos(opt,ppp,s)
+def removesqb(s):
+    lb=[]
+    brac=[]
+    b=0
+    for n,i in enumerate(s):
+        if i=='[' and b==0:
+            lb.append(n)
+            b+=1
+        elif i==']' and b==1:
+            brac.append((lb.pop(),n))
+            b-=1
+        elif i=='[':
+            b+=1
+        elif i==']':
+            b-=1
+    if len(brac)==0:
+        return s.strip()
     else:
-        fstr=s
-    return fstr
+        fst=s[:brac[0][0]]
+        for i in range(len(brac)-1):
+            fst+=s[brac[i][1]+1:brac[i+1][0]]
+        fst+=s[brac[-1][1]+1:]
+        return fst.strip()
 def stripemsub(s):
     s=s.replace("<em>","")
     s=s.replace("</em>","")
